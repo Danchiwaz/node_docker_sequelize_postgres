f112be0ce4171f48d7fd723d2a99c620d3e8dee0
Daniel Maina <mainadanielchiwaz@gmail.com>
2023-05-25 12:25:25 +0300

    added the crud

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,63 @@
 const User = require("../models/users.model");
 
-exports.getAll = async (req, res, next) =>{
+// gettting all the users
+exports.getAll = async (req, res, next) => {
+  try {
+    const allUsers = await User.findAll();
+    return res.status(200).json(allUsers);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// get one user
+
+exports.getOneUser = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const user = await User.findByPk(id);
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// create one user
+exports.createUser = async (req, res, next) => {
+  try {
+    const USER_MODEL = {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    };
     try {
-        const allUsers = await User.findAll();
-        return res.status(200).json(allUsers);
+      const user = await User.create(USER_MODEL);
+      return res.status(201).json({
+        message: "User created successfully",
+        data: user,
+      });
     } catch (error) {
-        return res.status(500).json({message: error.message});
+      return res.status(500).json({ error: error.message });
     }
-}
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
 
-
-
-// docker exec -it psql -U Danchiwaz node_docker_db 
+// delete user
+exports.deleteUser = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const user = await User.destory({
+      wherre: {
+        id: id,
+      },
+    });
+    return res.status(200).json({
+      message: "User deleted successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+// docker exec -it psql -U Danchiwaz node_docker_db
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,7 +2,8 @@ const userControllers = require("../controllers/users")
 const usersRouter = require("express").Router();
 
 
-usersRouter.route("/").get(userControllers.getAll)
+usersRouter.route("/").get(userControllers.getAll).post(userControllers.createUser)
+usersRouter.route("/:id").get(userControllers.getOneUser);
 
 
 // getAll 
